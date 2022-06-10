66123a7b7f2ffaa8485ebd49b16b934be4d4cf24
boboad3 <oad3jp999@gmail.com>
2022-06-11 07:01:16 +0900

    update

---
+++ add_manifest.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# ログ出力先
+LOG_PATH="$(pwd)" 
+
+# マニフェストファイル名
+TARGET_FILE='app-deployment.yml'
+
+# ログファイル名を設定。
+LOG_NAME="$(basename $0 | sed -e 's/.sh//g').log"
+
+# ログ関数
+function log () {
+  # ログ出力先とログ名を関数に設定。
+  LOG=$LOG_PATH/$LOG_NAME
+  # ログ出力日時のフォーマット yyyy/mm/dd hh:mm:ss 
+  time=$(date '+%Y/%m/%d %T')
+
+  # 最初の引数の文字列をログ出力
+  echo -e "$time" "$1" | tee -a $LOG
+
+  # 2番目の引数があればそれもログに出力（エラーログで使用）
+  if [[ $2 != "" ]]; then
+      echo -e "$2" | tee -a $LOG
+  fi   
+}
+
+# ヘルプ
+# ex) ./add_manifest.sh -a oad3jp999\\\/application-springboot 2.0.0
+function usage() {
+cat <<_EOT_
+Usage:
+  $0 [-a image_name tag]
+
+Description:
+  add manifest for arguments version
+
+Options:
+  -a          add manifest
+  -h (-help)  help
+
+_EOT_
+exit 1
+}
+
+# マニフェスト追加
+add_manifest() {
+
+  if [ "${1}" = "" -o "${2}" = "" ]; then
+    log "##### Insufficient arguments"
+    exit 1
+  fi
+
+  log "##### start #####"
+  log "##### image : ${1}"
+  log "##### tag   : ${2}"
+
+  log "##### make directory #####"
+  mkdir -p ${2}
+  target_dir="$(pwd)/${2}"
+  log "##### ${target_dir}"
+
+  log "##### copy manifest #####"
+  cp ./${TARGET_FILE} ./${2}/${TARGET_FILE}
+  log "##### $(ls -la ${target_dir})"
+
+  log "##### mod manifest #####"
+   sed -i "s/${1}.*/${1}:${2}/g" ./${2}/${TARGET_FILE}
+  log "##### $(grep ${1} ${target_dir}/${TARGET_FILE})"
+
+  exit 0
+}
+
+# オプション
+if [ "$OPTIND" = 1 ]; then
+  while getopts abf:h OPT
+  do
+    case $OPT in
+      a)
+        add_manifest ${2} ${3}
+        ;;
+      h)
+        echo "h option. display help"
+        usage
+        ;;
+      help)
+        echo "help option. display help"
+        usage
+        ;;
+      \?)
+        echo "Try to enter the h option." 1>&2
+        ;;
+    esac
+  done
+else
+  echo "No installed getopts-command." 1>&2
+  exit 1
+fi
